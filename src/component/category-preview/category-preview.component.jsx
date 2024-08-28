17f48d9b2a731540ef7f6920c9009d3d1b93ca59
Kulendran <kulendran.pathmanathan97@gmail.com>
2024-08-28 10:40:54 +0530

    Update category-preview.component

--- src/component/category-preview/category-preview.component.jsx
+++ src/component/category-preview/category-preview.component.jsx
@@ -1,26 +1,22 @@
-import { Link } from 'react-router-dom';
+import { useContext, Fragment } from 'react';
 
-import ProductCard from '../product-card/product-card.component';
+import { CategoriesContext } from '../../context/categories.context';
+import CategoryPreview from '../category-preview/category-preview.component';
 
-import './category-preview.styles.scss';
-
-const CategoryPreview = ({ title, products }) => {
+const CategoriesPreview = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+  console.log(categoriesMap);
+  console.log("called");
   return (
-    <div className='category-preview-container'>
-      <h2>
-        <Link className='title' to={title.toLowerCase()}>
-          {title}
-        </Link>
-      </h2>
-      <div className='preview'>
-        {products && products
-          .filter((_, idx) => idx < 4)
-          .map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </div>
+    <div>
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
     </div>
   );
 };
 
-export default CategoryPreview;
+export default CategoriesPreview;
